ef031c03a0631bae0bc4d2fa143d6e5f8d95e18c
hemit patel 🧑🏽‍💻 <hemitvpatel@gmail.com>
2025-09-01 15:03:58 -0400

    Create Variables.go

---
+++ Variables.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+  "fmt"
+)
+
+func main() {
+  // Create the variable jellybeanCounter
+  // here and make its type int8
+  const name string = "hemit"
+  fmt.Println(name)
+}
