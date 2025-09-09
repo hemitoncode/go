0fdea2db1a1e20293faf2a5cb5ca0dd5323cb619
hemit patel 🧑🏽‍💻 <hemitvpatel@gmail.com>
2025-09-08 21:36:50 -0400

    Update Conditional Logic.go

--- Conditional Logic.go
+++ Conditional Logic.go
@@ -36,4 +36,20 @@ func main() {
 	} else {
 		fmt.Println("Where did we go wrong?")
 	}
+
+// Same thing can be achieved here 
+	switch numOfThieves:=5; numOfThieves {
+		case 1:
+			fmt.Println("I'll take all $", amountStolen)
+		case 2:
+		  fmt.Println("Everyone gets $", amountStolen/2)
+		case 3:
+			fmt.Println("Everyone gets $", amountStolen/3)
+		case 4:
+		  fmt.Println("Everyone gets $", amountStolen/4)
+		case 5:
+			fmt.Println("Everyone gets $", amountStolen/5)
+		default:
+			fmt.Println("There's not enough to go around...")
+	}
 }
